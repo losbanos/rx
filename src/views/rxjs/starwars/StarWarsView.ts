0f3f06cd25105596f4f0ef3b7a1a2e9af7f7371f
KimTaeHoon <wwwholakr@gmail.com>
2020-04-01 22:51:39 +0900

    caculateBmi 함수 분리

--- src/views/rxjs/starwars/StarWarsView.ts
+++ src/views/rxjs/starwars/StarWarsView.ts
@@ -4,6 +4,8 @@ import ServiceInjectId from '@/const/ServiceInjectId';
 import {StarWarsService} from '@service/StarWarsService';
 import {IStarWarsPeople} from '@components/starwars/model/IStarWarsPeople';
 import {StarWarsMapper} from '@components/starwars/model/StarWarsMapper';
+import {Observable, from} from 'rxjs';
+import {pluck, filter} from 'rxjs/operators';
 
 @Component
 export default class StarWarsView extends Vue {
@@ -21,30 +23,45 @@ export default class StarWarsView extends Vue {
         this.starWarsService.load('people', 'json')
             .then(res => {
                 const results: Array<IStarWarsPeople> = StarWarsMapper.peopleMapper(res.results);
-                this.setPeopleInfo(results);
+                // this.setPeopleInfo(results);
+                this.setPeopleInfoByRx(results);
             });
     }
 
     protected setPeopleInfo(people: Array<IStarWarsPeople>) {
         this.items = people
             .filter((user: IStarWarsPeople) => /male|female/.test(user.gender))
-            .map((user) => {
+            .map(this.caculateBmi);
+    }
 
-                let broca: number = 0;
-                let bmi: number = 0;
+    protected setPeopleInfoByRx(people: Array<IStarWarsPeople>) {
+        const user$: Observable<IStarWarsPeople> = from(people);
+        user$.pipe(
+          filter(user => {
+              return user.gender === 'male';
+          })
+        ).subscribe(
+            n => {
+                this.items.push(n);
+            }
+        );
+    }
 
-                const height: number = user.height;
-                if (user.gender === 'male') {
-                    broca = Math.round(height - 100 * 0.9);
-                    bmi = Math.floor(height / 100 * height / 100 * 22);
-                } else {
-                    broca = Math.round(height - 100 * 0.9);
-                    bmi = Math.floor(height / 100  * height / 100 * 21);
-                }
-                const obesityUsingBroca: number = (user.mass - broca);
-                const obesityUsingBmi: number = Math.round((user.mass - bmi) / bmi * 100);
+    protected caculateBmi(user: IStarWarsPeople): IStarWarsPeople {
+        let broca: number = 0;
+        let bmi: number = 0;
 
-                return Object.assign({}, user, {broca, bmi, obesityUsingBroca, obesityUsingBmi});
-            });
+        const height: number = user.height;
+        if (user.gender === 'male') {
+            broca = Math.round(height - 100 * 0.9);
+            bmi = Math.floor(height / 100 * height / 100 * 22);
+        } else {
+            broca = Math.round(height - 100 * 0.9);
+            bmi = Math.floor(height / 100  * height / 100 * 21);
+        }
+        const obesityUsingBroca: number = (user.mass - broca);
+        const obesityUsingBmi: number = Math.round((user.mass - bmi) / bmi * 100);
+
+        return Object.assign({}, user, {broca, bmi, obesityUsingBroca, obesityUsingBmi});
     }
 }
