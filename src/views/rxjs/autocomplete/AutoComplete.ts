896b561dd136e3bf01beb5d24973df984a41d155
KimTaeHoon <wwwholakr@gmail.com>
2020-04-29 00:35:09 +0900

    divide keyup and users

--- src/views/rxjs/autocomplete/AutoComplete.ts
+++ src/views/rxjs/autocomplete/AutoComplete.ts
@@ -1,12 +1,16 @@
 import {Component, Vue} from 'vue-property-decorator';
-import {fromEvent, Observable, from, of} from 'rxjs';
+import {fromEvent, Observable, from, of, range, merge, partition} from 'rxjs';
 import {ajax} from 'rxjs/ajax';
-import {map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged} from 'rxjs/operators';
+import {map, mergeAll, mergeMap, debounceTime, filter, distinctUntilChanged, tap} from 'rxjs/operators';
+import {IResultItem} from './IResultItem';
 
 @Component
 export default class AutoComplete extends Vue {
 
     private GITHUB_URL: string = 'https://api.github.com/search';
+    private results: Array<IResultItem> = [];
+    private totalCount: number = 0;
+    private isLoading: boolean = false;
 
     protected mounted() {
         this.$nextTick(() => {
@@ -16,22 +20,37 @@ export default class AutoComplete extends Vue {
 
     private setInputEventHandler() {
 
-        const users$: Observable<Event> = fromEvent(this.$refs.search_inp as HTMLInputElement, 'keyup');
-        users$.pipe(
+        const keyup$: Observable<string> = fromEvent(this.$refs.search_inp as HTMLInputElement, 'keyup')
+        .pipe(
             debounceTime(1000),
             map((e: Event) => {
                 return (e.target as HTMLInputElement).value;
             }),
-            distinctUntilChanged(),
-            filter((key: string) => key.trim().length > 0),
+            distinctUntilChanged());
+
+        const [user$, reset$]: any = partition(keyup$, query => query.trim().length > 0);
+
+        user$.pipe(
+            tap(() => {
+                this.isLoading = true;
+            }),
             mergeMap((query: string) => {
                 return this.getUserRequest(this.GITHUB_URL, query);
+            }),
+            tap(() => {
+                this.isLoading = false;
             })
         ).subscribe((e: any) => {
-            console.log('result = ', e);
+            this.results = e.items;
+            this.totalCount = e.total_count;
         });
 
-        const request$: Observable<any> = this.getUserRequest(this.GITHUB_URL, '');
+        reset$.pipe(
+            tap(() => {
+                this.results = [];
+                this.totalCount = 0;
+            })
+        ).subscribe();
     }
 
     private getUserRequest(baseUrl: string, query: string): Observable<any> {
--- src/views/rxjs/autocomplete/AutoComplete.vue
+++ src/views/rxjs/autocomplete/AutoComplete.vue
@@ -1,6 +1,67 @@
 <template>
     <div id="autocomplete">
         <p><input type="input" id="search" placeholder="검색어 넣어" ref="search_inp"></p>
+        
+        <table v-if="!isLoading">
+            <thead>
+                <th scope="col">아이디</th>
+                <th scope="col">아바타</th>
+                <th scope="col">사용자명</th>
+                <th scope="col">홈페이지 주소</th>
+            </thead>
+            <tbody>
+                <tr v-for="item in results" :key="item.id">
+                    <td>{{item.id}}</td>
+                    <td><img :src="item.avatar_url" /></td>
+                    <td>{{item.login}}</td>
+                    <td><a :href="item.html_url" target="_blank">{{item.html_url}}</a></td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-else class="sbl-meter"></div>
+        <p>총 개수: {{totalCount}}</p>
     </div>
 </template>
 <script lang="ts" src="./AutoComplete.ts"></script>
+<style scoped>
+table{
+    border-collapse: collapse;
+}
+table tr{
+    border-bottom: 1px solid #aaa;
+}
+table tr:nth-child(odd) {
+    background-color: #eee;
+}
+img{
+    width: 60px;height: 60px;
+}
+  
+.sbl-meter {
+  height: 48px;
+  width: 48px;
+  color: #5a5a5a;
+  border-radius: 50%;
+  border: 5px solid;
+  position: relative;
+  display: inline-block; }
+  .sbl-meter::after {
+    content: '';
+    height: 25%;
+    width: 0;
+    border-radius: 5px;
+    border: 3px solid;
+    display: block;
+    color: inherit;
+    position: absolute;
+    left: 22px;
+    top: 5px;
+    transform-origin: bottom;
+    animation: rotate 1s linear infinite; }
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0); }
+  100% {
+    transform: rotate(360deg); } }
+</style>
